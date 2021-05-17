fff2ce055b1e1a9bc4f408b946609cbad174db9c
AleksandrTGH <73188277+AleksandrTGH@users.noreply.github.com>
2021-05-17 21:43:10 +0300

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car myFerrari = new Car("Lada","2107");
+        myFerrari.moveForward();
+        myFerrari.brake();
+    }
+}
